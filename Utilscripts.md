03e4ce75f38e0892bcaf2436f5e22d44bc10775d
MicamAmorim <kekoamorimsilva@gmail.com>
2025-01-24 20:30:32 -0300

    With Facial Features

--- Utilscripts.md
+++ Utilscripts.md
@@ -9,6 +9,7 @@ ___
 > Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
 
 ## Creating and using virtual envs
+> pip install virtualenv
 > python -m venv venv
 ### Activating venvs
 > venv/Scripts/activate
@@ -26,3 +27,12 @@ pip install -r requirements.txt
 > Cmake may be needed for face-recognition
 > Microsoft C++ compiling tools
 > May be necessary install dlib before using the wheel: dlib-19.24.1-cp311-cp311-win_amd64.whl
+
+# Install only this minimal requirements on your virtual env in this order
+> opencv-python==4.11.0.86
+> numpy==1.23.5
+> cmake==3.31.4
+- Change the path bellow to the path your file
+> dlib @ file:///C:/Users/Windows%2011/Documents/Projects/portifolio/FaceRecognition/dlib-19.24.1-cp311-cp311-win_amd64.whl#sha256=6f1a5ee167975d7952b28e0ce4495f1d9a77644761cf5720fb66d7c6188ae496
+> face-recognition==1.3.0
+
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import face_recognition
 import numpy as np
 # Carregar imagem com OpenCV
-image = cv2.imread("Data/barackObama.jpg")
+image = cv2.imread("Data/me.jpg")
 
 # Face Detection
 face_locations = face_recognition.face_locations(image)
@@ -15,6 +15,31 @@ for (top, right, bottom, left) in face_locations:
 cv2.imshow("Image", image)
 cv2.waitKey(0)
 
+cv2.destroyAllWindows()
+
+# Facial Landmarks Detection
+face_landmarks_list = face_recognition.face_landmarks(image)
+
+facial_features = [
+        'chin',
+        'left_eyebrow',
+        'right_eyebrow',
+        'nose_bridge',
+        'nose_tip',
+        'left_eye',
+        'right_eye',
+        'top_lip',
+        'bottom_lip']
+
+
+for face_landmarks in face_landmarks_list:
+    for facial_feature in facial_features:
+        for point in face_landmarks[facial_feature]:
+            image = cv2.circle(image, point, 2, (255,60,170),2)
+            
+            
+cv2.imshow("Image", image)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
